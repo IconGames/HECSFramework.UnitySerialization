ecd08e017dd604c35966d13136d9925fd7d1bda4
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-13 14:35:44 +0300

    upd: transform and vector3 serialization

---
+++ TransformComponent.cs
@@ -0,0 +1,26 @@
+﻿using HECSFramework.Core;
+
+namespace Components
+{
+    [GenerateResolver]
+    public partial class TransformComponent : IAfterSerializationComponent, IBeforeSerializationComponent
+    {
+        public void AfterSync()
+        {
+            if (Transform != null)
+            {
+                Transform.position = PositionSave.GetVector3();
+                Transform.rotation = UnityEngine.Quaternion.Euler(RotationSave.GetVector3());
+            }
+        }
+
+        public void BeforeSync()
+        {
+            if (Transform != null)
+            {
+                PositionSave.SetVector3Serialize(Transform.position);
+                RotationSave.SetVector3Serialize(Transform.rotation.eulerAngles);
+            }
+        }
+    }
+}
---
+++ Vector3Serialize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace HECSFramework.Core
+{
+    public partial struct Vector3Serialize
+    {
+        public Vector3 GetVector3()
+        {
+            return new Vector3(X, Y, Z);
+        }
+
+        public void SetVector3Serialize(Vector3 xyz)
+        {
+            X = xyz.x;
+            Y = xyz.y;
+            Z = xyz.z;
+        }
+    }
+}
