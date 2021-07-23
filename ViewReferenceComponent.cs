f02625f0de813351b6b938600ddf87ffe5cb4ded
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-23 16:43:55 +0300

    add: view ref component

---
+++ ViewReferenceComponent.cs
@@ -0,0 +1,12 @@
+﻿using HECSFramework.Core;
+
+namespace Components
+{
+    public partial class ViewReferenceComponent : BaseComponent, IBeforeSerializationComponent
+    {
+        public void BeforeSync()
+        {
+            this.AssetReferenceGuid = ViewReference.AssetGUID;
+        }
+    }
+}
