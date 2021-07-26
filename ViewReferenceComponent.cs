0d5beb0a9cf12660832d9beb6edc69b285ba2063
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-26 15:20:52 +0300

    upd: viewref serialization

--- ViewReferenceComponent.cs
+++ ViewReferenceComponent.cs
@@ -2,8 +2,13 @@
 
 namespace Components
 {
-    public partial class ViewReferenceComponent : BaseComponent, IBeforeSerializationComponent
+    public partial class ViewReferenceComponent : BaseComponent, IBeforeSerializationComponent, IAfterSerializationComponent
     {
+        public void AfterSync()
+        {
+            this.ViewReference = new ActorViewReference(AssetReferenceGuid);
+        }
+
         public void BeforeSync()
         {
             this.AssetReferenceGuid = ViewReference.AssetGUID;
