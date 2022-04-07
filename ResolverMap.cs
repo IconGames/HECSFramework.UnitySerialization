2acfee4c2f396971c120d4bb24ed95e0185a97af
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-04-08 02:42:02 +0300

    rem: dependency from msp to generated file

--- ResolverMap.cs
+++ ResolverMap.cs
@@ -11,22 +11,6 @@ namespace HECSFramework.Core
 {
     public partial class ResolversMap
     {
-        private static bool isMessagePackInited;
-        
-        static ResolversMap()
-        {
-            if (isMessagePackInited)
-                return;
-
-            if (!isMessagePackInited)
-                StaticCompositeResolver.Instance.Register(StandardResolver.Instance, GeneratedResolver.Instance);
-
-            isMessagePackInited = true;
-
-            var option = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
-            MessagePackSerializer.DefaultOptions = option;
-        }
-
         public async Task<IEntity> GetEntityFromResolver(EntityResolver entityResolver, bool needInitFromContainer, bool needForceAdd = false, int worldIndex = 0)
         {
             var unpack = new UnPackEntityResolver(entityResolver);
