23c195d0c6719f773901742a12e236f5fe20c5ee
Brightori <el.brightori@gmail.com>
2022-09-10 21:11:43 +0300

    upd: msp part of save manager

---
+++ Helpers/SaveManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using HECSFramework.Core;
+
+namespace HECSFramework.Unity
+{
+    public partial class SaveManager
+    {
+        public static bool TryLoadFromFile(string path, out EntityResolver entityResolver)
+        {
+            if (File.Exists(path))
+            {
+                FileStream fs = new FileStream(path, FileMode.Open);
+                try
+                {
+                    var loadData = MessagePack.MessagePackSerializer.Deserialize<EntityResolver>(fs);
+
+                    entityResolver = loadData;
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    HECSDebug.LogError("our data corrupted" + ex.Message);
+                }
+                finally
+                {
+                    fs.Close();
+                }
+            }
+
+            HECSDebug.LogWarning("нет файла сохранения");
+            entityResolver = default;
+            return false;
+        }
+    }
+}
