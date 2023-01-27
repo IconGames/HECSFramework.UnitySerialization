4fbe8695fb8e51992150e4188be4c5f15a9f10c2
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-01-27 14:36:09 +0300

    upd: vector serialize

---
+++ Helpers/VectorExtensions.cs
@@ -0,0 +1,11 @@
+﻿using HECSFramework.Core;
+using UnityEngine;
+
+namespace Helpers
+{
+    public static partial class VectorExtensions
+    {
+        public static Vector3 ToNumericsV3(this Vector3Serialize vector3)
+          => new Vector3(vector3.X, vector3.Y, vector3.Z);
+    }
+}
