3190fdd494f71d07602de93f94da8e0fbc230f71
Brightori <el.brightori@gmail.com>
2023-06-22 22:49:52 +0300

    mov: json part to json module

--- Components/SoundVolumeComponent.cs
+++ Components/SoundVolumeComponent.cs
@@ -1,8 +1,5 @@
-using HECSFramework.Serialize;
-
 namespace Components
 {
-    [JSONHECSSerialize]
     public sealed partial class SoundVolumeComponent: ISavebleComponent
     {
         [Field(0)]
